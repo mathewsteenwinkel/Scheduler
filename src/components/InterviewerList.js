2fdbd9a8dcb5d88cbd3947ddd5cfcb17f4f8f95e
Noidea22 <mathewsteenwinkel@gmail.com>
2023-04-19 19:45:50 -0600

    added components for the form

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "./InterviewerList.scss";
+
+import PropTypes from 'prop-types'; 
+
+function InterviewerList(props) {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">{props.name}</h4>
+      <ul className="interviewers__list">
+        {props.interviewers.map((interviewer) =>
+          <InterviewerListItem
+            key={interviewer.id}
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            selected={ props.value === interviewer.id }
+            setInterviewer={() => props.onChange(interviewer.id)}
+          />
+        )}
+      </ul>
+    </section>
+  );
+}
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
+
